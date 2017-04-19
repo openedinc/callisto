f40bf365e7ee54f39443fcc43a5c10450ed8d046
Adam Blum <adam@openedinstitute.org>
2017-04-19 13:20:15 -0700

    fixing action parsing

--- app/helpers/caliper_events_helper.rb
+++ app/helpers/caliper_events_helper.rb
@@ -3,7 +3,7 @@ module CaliperEventsHelper
     def action_from_event(event)
       p "Processing full payload #{event.payload.to_s}"
       payload_hash=JSON.parse(event.payload.to_s)
-      p "Action is: #{payload_hash['action']}"
+      p "Action is: #{payload_hash['data'][0]['action']}"
       payload_hash["action"]
     end
 
