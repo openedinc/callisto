1293f57bc89591b9768d7bb807b7fe20ef79b4d2
Adam Blum <adam@openedinstitute.org>
2017-04-19 12:17:59 -0700

    debugging failure to get action

--- app/helpers/caliper_events_helper.rb
+++ app/helpers/caliper_events_helper.rb
@@ -1,7 +1,9 @@
 module CaliperEventsHelper
 
     def action_from_event(event)
+      p "Processing #{event.payload}"
       payload_hash=JSON.parse(event.payload)
+      p "Processing #{payload_hash['action']}"
       payload_hash["action"]
     end
 
