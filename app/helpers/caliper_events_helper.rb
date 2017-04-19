1125b1c242dbd68db03a62acc0e3f77179893588
Adam Blum <adam@openedinstitute.org>
2017-04-19 13:39:16 -0700

    actor and object

--- app/helpers/caliper_events_helper.rb
+++ app/helpers/caliper_events_helper.rb
@@ -7,4 +7,17 @@ module CaliperEventsHelper
       payload_hash['data'][0]['action']
     end
 
+    def object_from_event
+      p "Processing full payload #{event.payload.to_s}"
+      payload_hash=JSON.parse(event.payload.to_s)
+      p "Action is: #{payload_hash['data'][0]['object']}"
+      payload_hash['data'][0]['object']
+    end
+
+    def actor_from_event
+      p "Processing full payload #{event.payload.to_s}"
+      payload_hash=JSON.parse(event.payload.to_s)
+      p "Action is: #{payload_hash['data'][0]['actor']}"
+      payload_hash['data'][0]['actor']
+    end
 end
