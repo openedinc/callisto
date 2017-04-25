7629e6285f530d21cbd08cf631c6b025c2087b8f
Adam Blum <adam@openedinstitute.org>
2017-04-25 12:14:37 -0700

    fix route for clear action

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   resources :assessment_events
   resources :media_events
-  resources :caliper_events
+  resources :caliper_events do
+    member do
+      get 'clear'
+    end
+  end
   resources :outcome_events
   resources :assessment_item_events
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
