77ff0dc1d2f6a0c451cc43f9e86318dbed97bae4
Lacides Charris <lcharris89@gmail.com>
2017-12-12 12:44:59 -0500

    Fix Readme.md rendering

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,9 @@
 class HomeController < ApplicationController
   skip_before_action :authenticate_user!
-  
+
   def index
     m = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
     content = m.render(File.open(Rails.root + "README.md", 'r').read)
-    render :text => content
+    render html: content.html_safe
   end
 end
