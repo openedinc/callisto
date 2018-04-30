5cd215ef0ed50e8dd3fdad8af9eb227765660431
Lars Burgess <larsburgess@users.noreply.github.com>
2018-04-30 16:22:50 -0700

    removing unsupported routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,11 @@ require_relative 'settings'
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      mount_devise_token_auth_for 'User', at: 'auth'
-      resources :assessment_events
-      resources :assessment_item_events
-      resources :grade_events
-      resources :media_events
+      mount_devise_token_auth_for 'User', at: 'auth', skip: [:registrations, :passwords]
+      get :assessment_events, to: 'assessment_events#index'
+      get :assessment_item_events, to: 'assessment_item_events#index'
+      get :grade_events, to: 'grade_events#index'
+      get :media_events, to: 'media_events#index'
     end
   end
 
